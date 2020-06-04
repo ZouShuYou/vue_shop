af8d0f8c89e22097b1dae584a9dbcecdb4073137
ZouShuYou <zsy4884@gmail.com>
2020-06-04 16:45:44 +0800

    完成报表功能

---
+++ src/components/report/Report.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>报表管理</el-breadcrumb-item>
+      <el-breadcrumb-item>报表列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <div id="main" style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+
+export default {
+  data() {
+    return {
+      // 需要合并的数据
+      options: {
+        title: {
+          text: "用户来源"
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross",
+            label: {
+              backgroundColor: "#E9EEF3"
+            }
+          }
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "3%",
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: "value"
+          }
+        ]
+      }
+    };
+  },
+  created() {},
+  async mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+    const { data: res } = await this.$http.get("reports/type/1");
+    if (res.meta.status !== 200) return this.$message("获取折线图数据失败!");
+    const result = _.merge(res.data, this.options);
+    // 5.展示数据
+    myChart.setOption(result);
+  },
+  method: {}
+};
+</script>
+
+<style lang="less" scoped>
+</style>
